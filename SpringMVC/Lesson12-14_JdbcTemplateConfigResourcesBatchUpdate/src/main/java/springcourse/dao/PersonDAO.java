95a6e2d89bff3c18b4f7719125c8b697e55078dd
NikitaV <nikita.volkov0505@mail.ru>
2022-07-03 17:43:51 +0300

    SpringMvc config BD in resources and added batch update

---
+++ SpringMVC/Lesson12-14_JdbcTemplateConfigResourcesBatchUpdate/src/main/java/springcourse/dao/PersonDAO.java
@@ -0,0 +1,102 @@
+package springcourse.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import springcourse.models.Person;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+// DAO Инкапсуляция источника данных. Проще говоря DAO урпавляет данными переданными из БД и тем самым закрывая потребность напрямую обращатся к БД
+@Component
+public class PersonDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)", person.getName(), person.getAge(),
+                person.getEmail());
+    }
+
+    public void update(int id, Person updatedPerson) {
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?", updatedPerson.getName(),
+                updatedPerson.getAge(), updatedPerson.getEmail(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+    }
+
+    ///////////////
+    //Тест производительности пакетной ставки
+    //////////////
+
+    public void testMutipleUpdate() {
+       List<Person> people = create1000people();
+
+       long before = System.currentTimeMillis();
+
+       // Обычное добавление 1000 пользователей. Делаем 1000 запросов к БД.
+       for(Person person : people) {
+           jdbcTemplate.update("INSERT INTO Person VALUES(?, ?, ?, ?)", person.getId(), person.getName(), person.getAge(),
+                   person.getEmail());
+       }
+
+       long after = System.currentTimeMillis();
+
+       System.out.println("time:" + (after - before));
+    }
+
+
+    // Добавление 1000 пользвателей с помощью BatchPreparedStatementSetter. Делаем 1 запрос к БД
+    public void testBatchUpdate() {
+        List<Person> people = create1000people();
+
+        long before = System.currentTimeMillis();
+
+        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES(?, ?, ?, ?)", new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setInt(1, people.get(i).getId()); // Установка значений в поля первый get(i) это получение
+                ps.setString(2, people.get(i).getName()); // индекса человека в массиве, последующие это получение
+                ps.setInt(3, people.get(i).getAge()); // Значений полей у этого человека
+                ps.setString(4, people.get(i).getEmail());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return people.size();
+            } // Получаем размер массива, соотвественно сколько людей нужно добавить
+        });
+
+        long after = System.currentTimeMillis();
+
+        System.out.println("time:" + (after - before));
+    }
+
+    public List<Person> create1000people() {
+        List<Person> people = new ArrayList<>();
+        for(int i = 0; i < 1000; i++) {
+            people.add(new Person(i, "Name" + i, 30, "Email" + i + "mail.ru"));
+        }
+        return people;
+    }
+}
