01be3f3e4f1386583807bca5d150f6bef25480d9
NikitaV <nikita.volkov0505@mail.ru>
2022-08-02 17:19:17 +0300

    Spring Boot Start

---
+++ SpringBoot/ProjectTwoBoot/src/test/java/springcourse/projecttwoboot/ProjectTwoBootApplicationTests.java
@@ -0,0 +1,13 @@
+package springcourse.projecttwoboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTwoBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
