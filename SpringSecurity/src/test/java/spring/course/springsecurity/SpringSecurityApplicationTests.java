f068b8e6f5e7fc9d3046237bc9412ddc0dfb0f67
NikitaV <nikita.volkov0505@mail.ru>
2022-08-13 21:36:55 +0300

    Spring Security

---
+++ SpringSecurity/src/test/java/spring/course/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.course.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
