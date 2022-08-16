2d53744486c5adb3b13a891e1e2e8c06eecb8656
NikitaV <nikita.volkov0505@mail.ru>
2022-08-16 18:46:20 +0300

    Spring Security: Configure

--- SpringSecurity/src/main/java/spring/course/springsecurity/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/spring/course/springsecurity/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package spring.course.springsecurity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -20,6 +21,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.personDetailsService = personDetailsService;
     }
 
+    // Настраиваем логику аутентификации
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) {
         try {
             auth.userDetailsService(personDetailsService);
@@ -27,6 +30,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             throw new RuntimeException(e);
         }
     }
+    // Настройка Spring Security(Какая страничка отвечает за вход, ошибки и тд)
+    // Конфигурируем авторизацию(Настройка прав у разных групп пользователей)
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Правила авторизации
+        http.authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated() // Настрока авторизации -> Все не авторизированные пользователи не имеют доступа к страничка
+                .and()// И... настройка кастомной странички входа
+                .formLogin().loginPage("/auth/login") // Передача нашей кастомной странички логина
+                .loginProcessingUrl("/process_login") // Адрес Post запроса
+                .defaultSuccessUrl("/hello", true) // На какую страничку перейти в случае успешного входа
+                .failureUrl("/auth/login?error"); // И в случае отрицательного результата входа
+    }
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
