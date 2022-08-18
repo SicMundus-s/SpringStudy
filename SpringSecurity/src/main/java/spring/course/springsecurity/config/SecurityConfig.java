413ffed4711bd0405cb6acd1e1f04b6b3a6069c3
NikitaV <nikita.volkov0505@mail.ru>
2022-08-18 19:25:41 +0300

    Spring Secuirty: CSRF

--- SpringSecurity/src/main/java/spring/course/springsecurity/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/spring/course/springsecurity/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // Правила авторизации
-        http.csrf().disable()
+        http
                 .authorizeRequests()
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().authenticated() // Настройка авторизации -> Все не авторизированные пользователи не имеют доступа к страничка
